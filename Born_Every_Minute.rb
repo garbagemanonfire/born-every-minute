0b756af3a57009e6b3dd54abb19c379ce0220f63
garbagemanonfire <nilesvm@gmail.com>
2013-10-26 16:33:04 -0700

    Added Styles and built remaining pages, 2/3 through increment 2

--- Born_Every_Minute.rb
+++ Born_Every_Minute.rb
@@ -1,30 +1,37 @@
 # ./Born_Every_Minute.rb
 require 'sinatra'
 require 'pry'
+# require 'sinatra/static_assets'
 
 # We us "classic" mode where you don't actually see the class
 # inheriting from Sinatra::Base and you don't start the app with
 # rackup. This makes the object structure less obvious but lets
 # Sinatra do more of the behind-the-scenes
 
-get '/' do
-  "Hello, World!"
-end
-
-post '/' do
-  "Caught a post" 
-end
-
 # get '/' do
-#   erb :home
+#   "Hello, World!"
 # end
 
-# get '/thanks' do
-#   erb :thanks
+# post '/' do
+#   "Caught a post" 
 # end
 
-# get '/suckers' do
-#   erb :suckers
+get '/' do
+  erb :home
+end
+
+get '/thanks' do
+  erb :thanks
+end
+
+get '/suckers' do
+  erb :suckers
+end
+
+# helpers do
+#   def img(name)
+#     "<img src='images/#{name}' alt='#{name}' />"
+#   end
 # end
 
 __END__
